657fca3176fdcdacf02bb1c2c49ade4211ec14a4
Łukasz Chmiel <chmiel_lukasz@interia.pl>
2019-04-09 08:06:23 +0200

    Defining Diary class

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+
+        //Stan (zmienne - pola)
+        List<float> ratings; // Lista wszystkich ocen
+
+
+        //Zachowania
+
+        public void AddRating(float rating) // Dodawanie nowej oceny do listy
+        {
+            ratings.Add(rating); 
+        }
+
+        public float CalculateAverage() // Wyliczanie i wyświetlanie średniej ocen
+        {
+            float sum = 0, avg = 0;
+            foreach (var item in ratings)
+            {
+                sum += item;
+            }
+
+            avg = sum / ratings.Count();
+
+            return avg;
+        }
+
+        public float GiveMaxRating() // Odnalezienie i wyświetlenie maksymalnej oceny
+        {
+           return ratings.Max();
+        }
+
+        public float GiveMinRating() // Odnalezienie i wyświetlenie minimalnej oceny
+        {
+            return ratings.Min();
+        }
+    }
+}
